5fa0ebd7dddc58bb8d0b7b02b17bbc94d595328f
wtp <15908282659@163.com>
2022-11-27 21:14:58 +0800

    add uboot tftp load

--- mx6ull_alientek_emmc.sh
+++ mx6ull_alientek_emmc.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+# bootargs=console=ttymxc0,115200 root=/dev/mmcblk1p2 rootwait rw
+# bootcmd=dhcp;tftp 80800000 zImage;tftp 83000000 imx6ull-alientek-emmc.dtb;bootz 80800000 - 83000000;
 make ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- distclean
 make ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- mx6ull_alientek_emmc_defconfig
 make V=1 ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf-
